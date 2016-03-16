12f00c7569729ad14063d2918bf4e288cf14886e
Ryan Bennett <ryan@filament.com>
2016-03-15 19:56:35 -0600

    add usb bridge

---
+++ bridge.js
@@ -0,0 +1,34 @@
+const serialport = require("serialport");
+const net = require('net');
+var shouldscan = true;
+
+const scan = module.exports = (args) => {
+  if (shouldscan){
+    serialport.list((err, ports) => {
+      if (err)
+        return err;
+      
+      ports.forEach((port) => {
+        if (port.vendorId == args.vendorId && port.productId == args.productId){
+          var sPort = new serialport.SerialPort(port.comName, {  baudrate: 115200}, false);
+          shouldscan = false;
+          sPort.open((err) => {
+            if (err)
+              return shouldscan = true;
+    
+            console.log("serial open")
+            var sock = net.connect(args.target)
+
+            sPort.pipe(sock)
+            sock.pipe(sPort)
+          })
+  
+          sPort.on('error', () => shouldscan = true)
+          sPort.on('close', () => shouldscan = true)
+        }
+      })
+    })
+  } 
+
+  setTimeout(() => scan(args), 5000)
+}
